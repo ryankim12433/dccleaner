00eafcf5579c93be42dfe9665c1c76dccb0ebbe3
ryankim12433 <88586799+ryankim12433@users.noreply.github.com>
2026-09-06 18:54:00 +0900

    Create PlatformVerticalScrollbar.kt

---
+++ composeApp/src/iosArm64Main/kotlin/com/dccleaner/app/platform composeApp/src/iosArm64Main/kotlin/com/dccleaner/app/ui/PlatformVerticalScrollbar.kt
@@ -0,0 +1,14 @@
+package com.dccleaner.app.ui.screen
+
+import androidx.compose.foundation.ScrollState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.dccleaner.app.model.UiColors
+
+@Composable
+actual fun PlatformVerticalScrollbar(
+    scrollState: ScrollState,
+    uiColors: UiColors,
+    modifier: Modifier
+) {
+}
